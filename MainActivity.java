88a65be179e55f704a56c544d2e03dd87b4c384b
Simple <88604455+futuretonight@users.noreply.github.com>
2024-02-19 22:38:38 +0530

    Update

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView adView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
+    }
+}
